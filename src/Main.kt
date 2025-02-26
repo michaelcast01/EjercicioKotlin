6cfd62f3c31cbe7d6d3697d093281aa6328575f7
michaelcast01 <michael2003cc@gmail.com>
2025-02-26 16:15:34 -0500

    Edificio

--- src/Main.kt
+++ src/Main.kt
@@ -1,44 +1,41 @@
 fun main() {
-    //  Variables
-    val dado1 = IntArray(10)
-    val dado2 = IntArray(10)
-    var suma1 = 0
-    var suma2 = 0
-    // Dado 1
-    println("Dado 1:")
-    for (i in 0..9) {
-        dado1[i] = (1..6).random()
-        println("Tirada ${i+1}: ${dado1[i]}")
-        suma1 += dado1[i]
-    }
-    // Dado 2
-    println("\nDado 2:")
-    for (i in 0..9) {
-        dado2[i] = (1..6).random()
-        println("Tirada ${i+1}: ${dado2[i]}")
-        suma2 += dado2[i]
-    }
-    // Mostrar resultados
-    println("\nValores del Dado 1:")
-    for (i in 0..9) {
-        print("${dado1[i]}")
-        if (i < 9) print(", ")
-    }
-    println("\nValores del Dado 2:")
-    for (i in 0..9) {
-        print("${dado2[i]}")
-        if (i < 9) print(", ")
-    }
-    // Mostrar sumas
-    println("\n\nSuma del Dado 1: $suma1")
-    println("Suma del Dado 2: $suma2")
-    // Mostrar ganador
-    println("\nResultado final:")
-    if (suma1 > suma2) {
-        println("¡Ganó el Dado 1!")
-    } else if (suma2 > suma1) {
-        println("¡Ganó el Dado 2!")
-    } else {
-        println("¡Empate!")
+    // Variable de fallos
+    var fallos = 0
+
+    // no puede pasar de 3 fallos
+    while (fallos < 3) {
+        print("Por favor, ingrese su código de acceso: ")
+        val entrada = readLine()
+        val codigoAcceso = entrada?.toIntOrNull()
+        if (codigoAcceso == null) {
+            fallos++
+            println("Código inválido. Intente nuevamente.")
+            continue
+        }
+        // Verifica el rango correspondiente
+        when (codigoAcceso) {
+            // Empleados (1000-1999)
+            in 1000..1999 -> {
+                println("Acceso autorizado. Bienvenido, empleado número: $codigoAcceso")
+            }
+            // Visitantes (2000-2999)
+            in 2000..2999 -> {
+                print("Ingrese su nombre: ")
+                val nombreVisitante = readLine() ?: ""
+                print("Ingrese el motivo de su visita: ")
+                val razonVisita = readLine() ?: ""
+                println("Acceso autorizado. Bienvenido, visitante $nombreVisitante. Motivo: $razonVisita")
+            }
+            // Códigos inválidos
+            else -> {
+                fallos++
+                // Bloquea si se superan 3 intentos
+                if (fallos == 3) {
+                    println("Acceso denegado. Se ha bloqueado el sistema por demasiados intentos.")
+                } else {
+                    println("Código incorrecto. Por favor, intente de nuevo.")
+                }
+            }
+        }
     }
 }
